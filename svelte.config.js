b8b40f1c6bdd41478403f026424c95042f0d36db
teds31 <tstanowski31@gmail.com>
2021-08-02 11:21:51 -0700

    netlify use version 12.2 node

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,10 @@
+const netlify = require('@sveltejs/adapter-netlify')
+
 import preprocess from "svelte-preprocess";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
+        adapter: netlify(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	},
